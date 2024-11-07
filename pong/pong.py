a808baeabdcd3f88f49f94f5aa915a39436ea7c5
Masaaki Oya <masaaki08@mac.com>
2024-11-07 10:49:50 +0900

    replace sound file

--- pong/pong.py
+++ pong/pong.py
@@ -1,36 +1,24 @@
 """
-Pongゲーム
+Simple Pong game
 """
 import sys
-from random import random, randint
-from math import sin, cos
-
 import pygame as pg
 
 W, H = 400, 300
-BALL_SPEED = 8
 
 pg.init()
 screen = pg.display.set_mode((W, H))
-
 clock = pg.time.Clock()
 
 pg.mixer.init()
-sound = pg.mixer.Sound('sounds/pico.mp3')
+sound = pg.mixer.Sound('sounds/pong.wav')
 
 def new_ball():
     ball = pg.Rect(W - 50, H // 2, 6, 6)
-    angle = randint(20, 40)
-    if random() < 0.5:
-        angle = 180 - angle
-    else:
-        angle = 180 + angle
-    angle = 3.14 * angle / 180
-    vx = BALL_SPEED * cos(angle)
-    vy = BALL_SPEED * sin(angle)
+    vx, vy = 5, 3
     return ball, vx, vy
 
-paddle = pg.Rect(10, H // 2, 10, 40)
+paddle = pg.Rect(10, H // 2, 5, 40)
 ball, ball_vx, ball_vy = new_ball()
 
 while True:
@@ -51,13 +39,13 @@ while True:
         ball_vy = -ball_vy
     ball.move_ip(ball_vx, ball_vy)
 
-    if ball.left < 0:
-        pg.time.wait(1000)
-        ball, ball_vx, ball_vy = new_ball()
-
     if ball_vx < 0 and ball.colliderect(paddle):
         ball_vx = -ball_vx
         sound.play()
+
+    if ball.left < 0:
+        pg.time.wait(1000)
+        ball, ball_vx, ball_vy = new_ball()
 
     screen.fill('darkgreen')
     pg.draw.rect(screen, 'skyblue', paddle)
